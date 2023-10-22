a8b41a85300a1df448f5d6a92bc097349b324736
Romildo Amaro <romildoamaro2@gmail.com>
2023-10-22 14:58:03 -0300

    Concluindo desafio

---
+++ script.js
@@ -0,0 +1,28 @@
+function getRank(victories, defeats) {
+    const winningBalance = victories - defeats;
+    let level;
+
+    if (victories < 10) {
+        level = "Ferro";
+    } else if (victories >= 11 && victories <= 20) {
+        level = "Bronze";
+    } else if (victories >= 21 && victories <= 50) {
+        level = "Prata";
+    } else if (victories >= 51 && victories <= 80) {
+        level = "Ouro";
+    } else if (victories >= 81 && victories <= 90) {
+        level = "Diamante";
+    } else if (victories >= 91 && victories <= 100) {
+        level = "Lendário";
+    } else {
+        level = "Imortal";
+    }
+
+    return `O Herói tem um saldo de ${winningBalance} vitórias e está no nível de ${level}`;
+}
+
+
+const victories = 210;
+const defeats = 50;
+const result = getRank(victories, defeats);
+console.log(result);
